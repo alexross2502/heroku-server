1b801962cd77db357f847c5e3daf40f7f27a98b7
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2022-06-16 13:50:45 +0300

    first12

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+const cors = require("cors");
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "working" });
 });
