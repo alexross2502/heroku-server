611f2f7eecf25741b907759692d863a0ae7c6266
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2022-06-27 19:24:11 +0300

    first26

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 const cors = require("cors");
-const sequelize = require("./db");
+//const sequelize = require("./db");
 //const models = require("./models/models");
 
 app.use(cors());
