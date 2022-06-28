baea03e0fa1ff2db8245cc17f491704694032ed3
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2022-06-28 18:28:56 +0300

    first45

--- db.js
+++ db.js
@@ -1,15 +1,7 @@
-var fs = require("fs");
-const Sequelize = require("sequelize");
+/*const Sequelize = require("sequelize");
 
 const db = new Sequelize(
-  "postgres://ititgzusuanudv:668ac3dcba26593a96c2a4e589f3328709eb7a16a0e07898032152537636c4c5@ec2-54-228-125-183.eu-west-1.compute.amazonaws.com:5432/d757up2m55747",
-  {
-    ssl: {
-      ca: fs.readFileSync(__dirname + "/server-ca.pem"),
-      cert: fs.readFileSync(__dirname + "/client-cert.pem"),
-      key: fs.readFileSync(__dirname + "/client-key.pem"),
-    },
-  }
+  "postgres://ititgzusuanudv:668ac3dcba26593a96c2a4e589f3328709eb7a16a0e07898032152537636c4c5@ec2-54-228-125-183.eu-west-1.compute.amazonaws.com:5432/d757up2m55747"
 );
 const data = db.define("anime", {
   title: Sequelize.STRING,
@@ -24,9 +16,8 @@ db.sync({ force: true }).then(() => {
 
 module.exports = {
   db,
-};
-
-/*const { Sequelize } = require("sequelize");
+};*/
+const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
   process.env.DATABASE_DB,
@@ -43,4 +34,4 @@ module.exports = new Sequelize(
       rejectUnauthorized: false,
     },
   }
-);*/
+);
